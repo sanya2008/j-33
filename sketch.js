b7b842b3e104ce0b9ed3fe5f0cd18d14de745c22
sanya2008 <74243016+sanya2008@users.noreply.github.com>
2021-05-21 15:47:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var boy
+var boy_jumping
+
+function preload(){
+  background(backgroundImg);
+  boy_jumping=loadAnimation("boy1-removebg-preview.png","boy2-removebg-preview.png")
+}
+
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  boy=createSprite(500,250,50,50)
+  boy.addAnimation("jumping",boy_jumping)
+
+}
+
+function draw() {
+  background(backgroundImg);
+  drawSprites();
+  Engine.update(engine);
+}
